c0891282da2d29761b9bc05db0ab64ce87a4aa85
John Gardner <gardner.john97+dphil@gmail.com>
2024-03-04 20:33:51 +0000

    text coverage

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,18 +1,27 @@
 from __future__ import annotations
 
+import pytest
 from ase import Atoms
 from ase.io import read
 from graph_pes.data import batch_graphs, convert_to_atomic_graphs
-from graph_pes.models.pairwise import LennardJones
+from graph_pes.models.zoo import LennardJones, Morse
 from graph_pes.training import Loss, train_model
 
+models = [
+    LennardJones(),
+    Morse(),
+]
 
-def test_integration():
+
+@pytest.mark.parametrize(
+    "model",
+    models,
+    ids=[model.__class__.__name__ for model in models],
+)
+def test_integration(model):
     structures: list[Atoms] = read("tests/test.xyz", ":")  # type: ignore
     graphs = convert_to_atomic_graphs(structures, cutoff=3)
     batch = batch_graphs(graphs)
-
-    model = LennardJones()
 
     loss = Loss("energy")
     before = loss(model.predict(batch), batch)
