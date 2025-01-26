fix numerical instabilities (#110)

src/graph_pes/models/painn.py
@@ -142,10 +142,8 @@ class Update(nn.Module):
         v = self.V(vector_embeddings)  # (N, D, 3)
 
         # stack scalar message and the norm of v
-        m = torch.cat(
-            [scalar_embeddings, torch.linalg.norm(v, dim=-1)],
-            dim=-1,
-        )  # (N, 2D)
+        vnorm = torch.sqrt(torch.sum(v**2, dim=-1) + 1e-8)
+        m = torch.cat([scalar_embeddings, vnorm], dim=-1)  # (N, 2D)
         m = self.mlp(m)  # (N, 3D)
 
         # split the update into 3 parts
@@ -243,27 +241,24 @@ class PaiNN(GraphPESModel):
     def forward(self, graph: AtomicGraph) -> dict[PropertyKey, Tensor]:
         # initialise embbedings:
         # - scalars as an embedding of the atomic numbers
-        scalar_embeddings = self.z_embedding(graph.Z)
+        scalars = self.z_embedding(graph.Z)
         # - vectors as all 0s:
-        vector_embeddings = torch.zeros(
-            (number_of_atoms(graph), self.channels, 3),
-            device=graph.Z.device,
+        vectors = torch.zeros(
+            (number_of_atoms(graph), self.channels, 3), device=graph.Z.device
         )
 
         # iteratively interact and update the scalar and vector embeddings
         for interaction, update in zip(self.interactions, self.updates):
-            delta_v, delta_s = interaction(
-                vector_embeddings, scalar_embeddings, graph
-            )
-            vector_embeddings = vector_embeddings + delta_v
-            scalar_embeddings = scalar_embeddings + delta_s
+            dv, ds = interaction(vectors, scalars, graph)
+            vectors = vectors + dv
+            scalars = scalars + ds
 
-            delta_v, delta_s = update(vector_embeddings, scalar_embeddings)
-            vector_embeddings = vector_embeddings + delta_v
-            scalar_embeddings = scalar_embeddings + delta_s
+            dv, ds = update(vectors, scalars)
+            vectors = vectors + dv
+            scalars = scalars + ds
 
         # mlp read out
-        local_energies = self.read_out(scalar_embeddings).squeeze()
+        local_energies = self.read_out(scalars).squeeze()
 
         # scaling
         local_energies = self.scaler(local_energies, graph)

src/graph_pes/models/tensornet.py
@@ -218,7 +218,7 @@ class EdgeEmbedding(nn.Module):
         # 2. encode atomic species of ordered neighbour pairs:
         h_z_atom = self.z_embedding(graph.Z)  # (N, C)
         h_z_edge = h_z_atom[graph.neighbour_list]  # (2, E, C)
-        h_z_edge = h_z_edge.reshape(E, 2 * C)
+        h_z_edge = h_z_edge.permute(1, 0, 2).reshape(E, 2 * C)
         h_z_edge = self.z_map(h_z_edge)  # (E, C)
 
         # 3. embed edge distances
@@ -355,7 +355,7 @@ class Interaction(nn.Module):
         Y = Y @ M_i + M_i @ Y  # (N, C, 3, 3)
 
         # renormalise and decompose
-        norm = frobenius_norm(Y + 1)[..., None, None]  # (N, C, 1, 1)
+        norm = (frobenius_norm(Y) + 1)[..., None, None]  # (N, C, 1, 1)
         I, A, S = decompose_matrix(Y / norm)  # (N, C, 3, 3)
 
         # mix features again

tests/models/test_equivariance.py
@@ -4,7 +4,6 @@ import torch
 from ase.build import molecule
 
 from graph_pes import AtomicGraph, GraphPESModel
-from graph_pes.models import PaiNN
 
 from .. import helpers
 
@@ -13,9 +12,6 @@ CUTOFF = 1.2  # bond length of methane is ~1.09 Å
 
 @helpers.parameterise_all_models(expected_elements=["H", "C"], cutoff=CUTOFF)
 def test_equivariance(model: GraphPESModel):
-    if isinstance(model, (PaiNN,)):
-        pytest.skip(f"TODO: fix {model.__class__.__name__} equivariance")
-
     methane = molecule("CH4")
     og_graph = AtomicGraph.from_ase(methane, cutoff=CUTOFF)
     og_predictions = model.get_all_PES_predictions(og_graph)
@@ -41,16 +37,16 @@ def test_equivariance(model: GraphPESModel):
     torch.testing.assert_close(
         og_predictions["energy"],
         new_predictions["energy"],
-        atol=1e-5,
-        rtol=1e-5,
+        atol=2e-5,
+        rtol=1e-3,
     )
 
     # 2. invariance of force magnitude
     torch.testing.assert_close(
         og_predictions["forces"].norm(dim=-1),
         new_predictions["forces"].norm(dim=-1),
-        atol=1e-5,
-        rtol=1e-5,
+        atol=2e-5,
+        rtol=1e-3,
     )
 
     # 3. equivariance of forces
@@ -68,12 +64,17 @@ def test_equivariance(model: GraphPESModel):
     )
 
     # 4. molecule is symetric: forces should ~0 on the central C,
-    #    and of equal magnitude on the H atoms
+    #    and of equal magnitude on the H atoms, and H atom
+    #    local energies should be the same
     force_norms = new_predictions["forces"].norm(dim=-1)
     c_force = force_norms[new_graph.Z == 6]
     assert c_force.item() == pytest.approx(0.0, abs=3e-4)
 
     h_forces = force_norms[new_graph.Z == 1]
     assert h_forces.min().item() == pytest.approx(
-        h_forces.max().item(), abs=3e-3
+        h_forces.max().item(), abs=1e-4
     )
+
+    h_local_energies = new_predictions["local_energies"][new_graph.Z == 1]
+    _min, _max = h_local_energies.min().item(), h_local_energies.max().item()
+    assert _min == pytest.approx(_max, abs=1e-6)