2fe1d7a578511f61231f8298db4a4574194e0fed
John Gardner <gardner.john97+dphil@gmail.com>
2025-02-05 14:16:31 +0000

    bump 0.0.21 -> 0.0.22

--- src/graph_pes/graph_pes_model.py
+++ src/graph_pes/graph_pes_model.py
@@ -104,7 +104,7 @@ class GraphPESModel(nn.Module, ABC):
     ):
         super().__init__()
 
-        self._GRAPH_PES_VERSION: Final[str] = "0.0.21"
+        self._GRAPH_PES_VERSION: Final[str] = "0.0.22"
 
         self.cutoff: torch.Tensor
         self.register_buffer("cutoff", torch.tensor(cutoff))
