26e739cc72564fd8c260ab0a58c5a6a49d362a33
John Gardner <gardner.john97+dphil@gmail.com>
2025-04-10 18:15:32 +0100

    fix e3nn loading

--- src/graph_pes/__init__.py
+++ src/graph_pes/__init__.py
@@ -1,10 +1,15 @@
 import warnings
 
+import torch
+
 from graph_pes.atomic_graph import AtomicGraph
 from graph_pes.graph_pes_model import GraphPESModel
 
 # hide the annoying FutureWarning from e3nn
 warnings.filterwarnings("ignore", category=FutureWarning, module="e3nn")
+
+# fix e3nns torch.load without weights_only
+torch.serialization.add_safe_globals([slice])
 
 __all__ = ["AtomicGraph", "GraphPESModel"]
 __version__ = "0.0.33"
