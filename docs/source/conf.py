8738abf6db744aa3cf69f3d6ad883a21ac767318
John Gardner <gardner.john97+dphil@gmail.com>
2025-04-10 18:41:25 +0100

    bump 0.0.33 -> 0.0.34

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 project = "graph-pes"
 copyright = "2023-2024, John Gardner"
 author = "John Gardner"
-release = "0.0.33"
+release = "0.0.34"
 
 extensions = [
     "sphinx.ext.duration",
--- src/graph_pes/graph_pes_model.py
+++ src/graph_pes/graph_pes_model.py
@@ -126,7 +126,7 @@ class GraphPESModel(nn.Module, ABC):
     ):
         super().__init__()
 
-        self._GRAPH_PES_VERSION: Final[str] = "0.0.33"
+        self._GRAPH_PES_VERSION: Final[str] = "0.0.34"
 
         self.cutoff: torch.Tensor
         self.register_buffer("cutoff", torch.tensor(cutoff))
